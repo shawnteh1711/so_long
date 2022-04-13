0881eee19c844e567e44258cbc3d820dc48d3808
Shawn Teh <steh@u91z06s03.42kl.edu.my>
2022-04-13 20:45:30 +0800

    fix can walk through wall

--- close.c
+++ close.c
@@ -6,7 +6,7 @@
 /*   By: steh <steh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 17:17:46 by steh              #+#    #+#             */
-/*   Updated: 2022/04/13 19:02:46 by steh             ###   ########.fr       */
+/*   Updated: 2022/04/13 20:44:22 by steh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@ int	key_hook(int keycode, t_data *g)
 		g->player.next_step.y = g->player.next_step.y + 1;
 	else
 		return (0);
-	if (keycode == LEFT || keycode == RIGHT
-		|| keycode == UP || keycode == DOWN)
-		g->player.moves += 1;
 	if (g->map.maparray[g->player.next_step.y][g->player.next_step.x] != '1')
 		move(g);
+	else
+	{
+		g->player.next_step.x = g->player.pre_step.x;
+		g->player.next_step.y = g->player.pre_step.y;
+	}
 	return (0);
 }
 
--- gameplay.c
+++ gameplay.c
@@ -6,7 +6,7 @@
 /*   By: steh <steh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 19:01:26 by steh              #+#    #+#             */
-/*   Updated: 2022/04/13 19:11:09 by steh             ###   ########.fr       */
+/*   Updated: 2022/04/13 20:43:54 by steh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,16 @@ void	move(t_data *g)
 
 	if (g->map.maparray[g->player.next_step.y][g->player.next_step.x] == 'C')
 		g->map.coin -= 1; 
-	if (g->map.maparray[g->player.next_step.y][g->player.next_step.x] == 'E'
-		&& g->map.coin == 0)
+	if ((g->map.maparray[g->player.next_step.y][g->player.next_step.x] == 'E'
+		&& g->map.coin == 0) 
+		|| g->map.maparray[g->player.next_step.y][g->player.next_step.x] == 'G')
 		close_game(g);
 	else if (g->map.maparray[g->player.next_step.y][g->player.next_step.x] == 'E')
 	{
 		message = "Please collect all coins";
-		mlx_string_put(g->mlx, g->win, 0, 0, 0x00FFFFFF, message);
-	}
+		mlx_string_put(g->mlx, g->win, 100, 100, 0x00FFFFFF, message);
+	} 
+	g->player.moves += 1;
 	edit_array(g);
 	mlx_clear_window(g->mlx, g->win);
 	put_image(g);
